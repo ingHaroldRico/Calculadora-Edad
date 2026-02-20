532bf8c3306f6ee959c5d96b2f2b304465ebeccf
Harold <ingenieroharold2013@gmail.com>
2026-02-20 13:31:57 -0500

    Validaciones mejoradas

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const result=document.getElementById("resultado")
 
 function calcularEdad(){
     let datoNacimento=new Date (fe_nacimiento.value)
+
     let añoNacimiento=datoNacimento.getFullYear()
-   
     let añoActual=new Date().getFullYear()
 
-     if (añoNacimiento >=añoActual){
+
+
+
+    if (añoNacimiento >=añoActual){
         alert("Ingresa una fecha valida")
         result.innerText="Año Ingresado debe ser menor al Año actual"
         return
@@ -19,10 +22,22 @@ function calcularEdad(){
         return
 
     }
-    else{
+    let meses=new Date().getMonth()-datoNacimento.getMonth()
+    let dias=new Date().getDate()-datoNacimento.getDate()
+    let edad=añoActual-añoNacimiento
 
-        let edad=añoActual-añoNacimiento
+    if(meses<0 || (meses==0 && dias <= 0)){
+        edad=edad-1
         result.innerText="Tu edad es " + edad +" Años"
+
+        // let edad=añoActual-añoNaceimiento
+        // result.innerText="Tu edad es " + edad +" Años"
+    }
+    else{
+    
+        result.innerText="Tu edad es " + edad +" Años"
+
     }
     
+   
 }
