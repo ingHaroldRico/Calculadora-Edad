c42aa6b1756a7516d0455ee02771385e1f0f040d
Harold <ingenieroharold2013@gmail.com>
2026-02-23 07:46:01 -0500

    con funciones agregadas

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 const fe_nacimiento=document.getElementById("f_nacimiento")
 const result=document.getElementById("resultado")
 
+function mostrar(edad){
+    result.innerText="Tu edad es " + edad +" Años"
+}
+function validacion(añoNacimiento,añoActual){
+    if (añoNacimiento >= añoActual){
+        alert("Ingresa una fecha valida")
+        result.innerText="Año Ingresado debe ser menor al Año actual"
+        return false
+    }
+    if(isNaN(añoNacimiento)){
+        alert("Ingresa una fecha valida")
+        result.innerText="Debes Seleccionar todos los Datos de tu nacimiento"
+        return false
+    }
+    return true
+}
+
 function calcularEdad(){
     let datoNacimento=new Date (fe_nacimiento.value)
 
@@ -9,34 +26,25 @@ function calcularEdad(){
     let añoActual=new Date().getFullYear()
 
 
-
-
-    if (añoNacimiento >=añoActual){
-        alert("Ingresa una fecha valida")
-        result.innerText="Año Ingresado debe ser menor al Año actual"
-        return
+    //jj
+    if(validacion(añoNacimiento,añoActual)==false){
+        return 
     }
-    if(isNaN(añoNacimiento)){
-        alert("Ingresa una fecha valida")
-        result.innerText="Debes Seleccionar todos los Datos de tu nacimiento"
-        return
 
-    }
+
     let meses=new Date().getMonth()-datoNacimento.getMonth()
     let dias=new Date().getDate()-datoNacimento.getDate()
     let edad=añoActual-añoNacimiento
 
     if(meses<0 || (meses==0 && dias <= 0)){
         edad=edad-1
-        result.innerText="Tu edad es " + edad +" Años"
+        mostrar(edad)
 
         // let edad=añoActual-añoNaceimiento
         // result.innerText="Tu edad es " + edad +" Años"
     }
     else{
-    
-        result.innerText="Tu edad es " + edad +" Años"
-
+        mostrar(edad)
     }
     
    
